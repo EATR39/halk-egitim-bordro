032fdda88bbff13201c28dd1e28417fe08e626a1
EATR39 <emrahanlak@gmail.com>
2025-11-27 20:52:21 +0300

    Create KURULUM.bat

---
+++ KURULUM.bat
@@ -0,0 +1,8 @@
+@echo off
+rem Windows Batch File for Automatic Installation
+
+echo Starting installation...
+echo Installing required packages...
+rem Here you would add commands to install necessary packages
+
+echo Installation complete!
